d3ddedd8de073eb347eb0114d716ab18bdd63fd9
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2025-11-25 16:06:45 +0700

    add config pm2

---
+++ ecosystem.config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "wms-ymid-dashboard",
+            script: "node_modules/next/dist/bin/next",
+            args: "start -p 4801",
+            cwd: "D:/Source/WMS YMID/wms-dashboard",
+            watch: false
+        },
+    ]
+};
